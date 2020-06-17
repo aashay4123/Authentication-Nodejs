f80d55224549845df26dc593b5c425e22fb433db
Aashay <aashayphirke5798@gmail.com>
2020-06-17 13:58:19 +0530

    improve

---
+++ client/src/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  REACT_APP_API: "http://localhost:8000/api",
+  REACT_APP_GOOGLE_CLIENT_ID:
+    "303808496602-pi3vphdua3ib3h35803857ipt10gvq20.apps.googleusercontent.com",
+  REACT_APP_FACEBOOK_ID: "2032472200230249",
+  REACT_APP_JWT_SECRET="12356987484747175452fsdbvdsghGHCFSDXFAsd4s854",
+
+};
--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,6 +1,7 @@
 module.exports = {
-  REACT_APP_API: "http://localhost:8000/api",
-  REACT_APP_GOOGLE_CLIENT_ID:
-    "303808496602-pi3vphdua3ib3h35803857ipt10gvq20.apps.googleusercontent.com",
-  REACT_APP_FACEBOOK_ID: "2032472200230249",
+  REACT_APP_API: process.env.REACT_APP_API,
+  REACT_APP_GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  REACT_APP_FACEBOOK_ID: process.env.REACT_APP_FACEBOOK_ID,
+  REACT_APP_JWT_SECRET= process.env.REACT_APP_JWT_SECRET,
+
 };
