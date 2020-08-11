a7e03448e78b45497e669ecec81ac1e6ffee519c
aashay4123 <aashayphirke5798@gmail.com>
2020-08-11 21:42:43 +0530

    production error

--- client/src/container/private.js
+++ client/src/container/private.js
@@ -35,7 +35,7 @@ const Private = ({ history }) => {
         setValues({ ...values, role, name, email });
       })
       .catch((error) => {
-        console.log("PRIVATE PROFILE UPDATE ERROR", error.response.data.error);
+        console.log("PRIVATE PROFILE UPDATE ERROR", error.response);
         if (error.response.status === 401) {
           signout(() => {
             history.push("/");
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.Promise = global.Promise;
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.redirect("https://" + req.headers.host + req.url);
   });
