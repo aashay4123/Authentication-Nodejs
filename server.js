8e12f2f5dce8d242d85164913814756d579e9219
aashay4123 <aashayphirke5798@gmail.com>
2020-08-11 22:34:25 +0530

    production error

--- server.js
+++ server.js
@@ -24,14 +24,18 @@ mongoose
 mongoose.Promise = global.Promise;
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
