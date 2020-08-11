ebcc01b3c03791c1731ddd4cb6141505ff4e74f3
aashay4123 <aashayphirke5798@gmail.com>
2020-08-11 21:20:02 +0530

    production error

--- client/src/container/layout.js
+++ client/src/container/layout.js
@@ -3,42 +3,42 @@ import { withRouter } from "react-router-dom";
 import Header from "../components/header";
 
 const Layout = ({ children, match, history }) => {
-  if (process.env.NODE_ENV === "production") {
-    let deferredPrompt;
-    if (process.browser) {
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker
-          .register("/sw.js")
-          .then(function () {
-            console.log("service worker registered");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-      window.addEventListener("beforeinstallprompt", function (event) {
-        event.preventDefault();
-        console.log("prompt");
-        deferredPrompt = event;
-        return false;
-      });
-    }
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
+  // if (process.env.NODE_ENV === "production") {
+  //   let deferredPrompt;
+  //   if (process.browser) {
+  //     if ("serviceWorker" in navigator) {
+  //       navigator.serviceWorker
+  //         .register("/sw.js")
+  //         .then(function () {
+  //           console.log("service worker registered");
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         });
+  //     }
+  //     window.addEventListener("beforeinstallprompt", function (event) {
+  //       event.preventDefault();
+  //       console.log("prompt");
+  //       deferredPrompt = event;
+  //       return false;
+  //     });
+  //   }
+  //   if (deferredPrompt) {
+  //     deferredPrompt.prompt();
 
-      deferredPrompt.userChoice.then(function (choiceResult) {
-        console.log(choiceResult.outcome);
+  //     deferredPrompt.userChoice.then(function (choiceResult) {
+  //       console.log(choiceResult.outcome);
 
-        if (choiceResult.outcome === "dismissed") {
-          console.log("User cancelled installation");
-        } else {
-          console.log("User added to home screen");
-        }
-      });
+  //       if (choiceResult.outcome === "dismissed") {
+  //         console.log("User cancelled installation");
+  //       } else {
+  //         console.log("User added to home screen");
+  //       }
+  //     });
 
-      deferredPrompt = null;
-    }
-  }
+  //     deferredPrompt = null;
+  //   }
+  // }
 
   return (
     <Fragment>
